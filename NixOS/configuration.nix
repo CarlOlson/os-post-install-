656f9282688c1c71a2d205668009d91bc35a46e8
Carl Olson <CarlOlson@users.noreply.github.com>
2017-08-05 18:06:13 -0500

    add nixos configuration

---
+++ NixOS/configuration.nix
@@ -0,0 +1,143 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  system.stateVersion = "17.03";
+
+  networking.hostName = "nixos";
+
+  nixpkgs.config.allowUnfree = true;
+
+  i18n = {
+    consoleFont = "anonymousPro";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  time.timeZone = "US/Central";
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sdb";
+  boot.loader.grub.useOSProber = true;
+
+  environment.systemPackages = with pkgs; [
+    coreutils git nox oh-my-zsh tree ts wget zsh
+    p7zip unrar unzip
+    ffmpeg-full youtube-dl
+
+    emacs25 pavucontrol qbittorrent yakuake
+
+    exfat ntfs3g
+  ];
+
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    autorun = true;
+    videoDrivers = [ "ati" ];
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+  users.extraUsers.carl = {
+    isNormalUser = true;
+    home = "/home/carl";
+    description = "Carl Olson";
+    extraGroups = [ "wheel" ];
+  };
+
+  sound.mediaKeys = {
+    enable = true;
+    volumeStep = "5%";
+  };
+
+  hardware.pulseaudio.enable = true;
+
+  nix.maxJobs = 4;
+  nix.buildCores = 4;
+
+  programs.chromium = {
+    enable = true;
+    extensions = [
+      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
+      "pgdnlhfefecpicbbihgmbmffkjpaplco" # ublock origin extra
+      "dhdgffkkebhmkfjojejmpbldmpobfkfo" # tampermonkey stable
+      "hdokiejnpimakedhajhdlcegeplioahd" # lastpass
+    ];
+    extraOpts = {
+      DefaultPluginsSetting = 3; # click to run flash
+      PasswordManagerEnabled = false;
+      RestoreOnStartup = 1; # restore session
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = ["git" "wd"];
+      theme = "sunrise";
+    };
+
+    syntax-highlighting = {
+      enable = true;
+      highlighters = ["main" "brackets"];
+    };
+  };
+
+  fonts = {
+    enableFontDir = true;
+    enableCoreFonts = true;
+    enableGhostscriptFonts = true;
+    fonts = with pkgs; [
+      anonymousPro
+      bakoma_ttf
+      corefonts
+      dejavu_fonts
+      gentium
+      liberation_ttf
+      inconsolata
+      terminus_font
+      ubuntu_font_family
+    ];
+  };
+
+  services.samba.enable = true;
+  services.samba.shares = {
+    samba = {
+      path = "/samba";
+      "read only" = "yes";
+      writable = "no";
+      public = "yes";
+      browsable = "yes";
+      "guest ok" = "yes";
+    };
+  };
+
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [
+    139 445 # samba
+  ];
+  networking.firewall.allowedUDPPorts = [
+    137 138 # samba
+  ];
+
+  fileSystems."/mnt/Backup" = {
+    device = "/dev/disk/by-label/Backup";
+    fsType = "ntfs";
+  };
+
+  fileSystems."/mnt/Music" = {
+    device = "/dev/disk/by-label/Music";
+  };
+
+  fileSystems."/mnt/Video" = {
+    device = "/dev/disk/by-uuid/F220C70420C6CF33";
+    fsType = "ntfs";
+  };
+}
